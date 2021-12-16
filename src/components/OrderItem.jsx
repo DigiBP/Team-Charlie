e67874ff9e89c9152b61e4b5d38a876c2b552db0
Gabriel MassaroFHNW <gabriel.massaro@students.fhnw.ch>
2021-12-16 10:18:06 +0100

    added not in stock and warning icon for prescriptive drugs

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,16 +1,31 @@
+import { useEffect, useRef } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
+
 function OrderItem({ orderItem, orderUpdateHandler }) {
   return (
     <div className="item-card col-12 col-sm-4 mb-4 mt-3">
       <div className="p-3 border bg-light text-center">
-        <p className="h5 fw-bold">{orderItem.fields.Name}</p>
+        <p className="h5 fw-bold">
+          {orderItem.fields.Name}
+          {orderItem.fields.IsOtc ? null : <span className='text-warning' title="needs prescription"> <FontAwesomeIcon icon={faExclamationTriangle} /></span>}
+        </p>
         <p>{orderItem.fields.Price} CHF</p>
         <div className="clearfix d-flex justify-content-center">
-          <button
-            className="btn btn-outline-success"
-            onClick={() => orderUpdateHandler("drugName", orderItem.fields.Name)}
-          >
-            Order {orderItem.fields.Name}
-          </button>
+          {orderItem.fields.AmountInStock > 0 ?
+            <button
+              className="btn btn-outline-success"
+              onClick={() => orderUpdateHandler("drugName", orderItem.fields.Name)}
+            >
+              Order
+            </button>
+            :
+            <button
+              className="btn btn-outline-danger disbaled"
+            >
+              Not in stock
+            </button>
+          }
         </div>
       </div>
     </div>
